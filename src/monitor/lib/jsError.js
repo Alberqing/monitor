c676f39123a728ff38e67e987c78c12a73e21d76
zhengyunqing <zhengyunqing@kuaishou.com>
2020-07-14 14:39:18 +0800

    promise error

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -17,8 +17,40 @@ export function injectJsError() {
             position: `${event.lineno}:${event.colno}`,
             selector: lastEvent ? getSelector(lastEvent.path) : "", // 代表最后操作的元素
         }
-        console.log(log, "log");
         tracker.send(log)
+    })
+
+    window.addEventListener("unhandledrejection", function(event) {
+        console.log(event);
+        let lastEvent = getLastEvent(); //最后一个交互事件
+        let message;
+        let filename;
+        let line = 0;
+        let col = 0;
+        let stack = "";
+        let reason = event.reason;
+        if(typeof reason === "string") {
+            message = reason
+        } else if(typeof reason === "object") {
+            message = reason.message
+            if(reason.stack) {
+                let matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/)
+                filename = matchResult[1];
+                line = matchResult[2];
+                col = matchResult[3];
+            }
+            stack = getLines(reason.stack);
+        }
+        tracker.send({
+            kind: "stability", // 监控指标的大类
+            type: "error",   // 小类型 错误
+            errorType: "promiseError", // js执行错误
+            message, // 报错信息
+            stack,
+            filename, //哪个文件报错
+            position: `${line}:${col}`,
+            selector: lastEvent ? getSelector(lastEvent.path) : "", // 代表最后操作的元素
+        })
     })
  
     function getLines(stack) {
--- src/monitor/utils/tracker.js
+++ src/monitor/utils/tracker.js
@@ -7,9 +7,9 @@ let logStore = "zyq-monitor-storage";
 function getExtraData() {
     return {
         title: document.title,
-        url: location.url,
+        url: location.href,
         timestamp: Date.now(),
-        userAgent: userAgent.parse(navigator.userAgent),
+        userAgent: userAgent.parse(navigator.userAgent).name,
     }
 }
 
@@ -21,22 +21,25 @@ class SendTracker {
     send(data = {}) {
         let extraData = getExtraData();
         let log = {...extraData, ...data};
+        // 对象不能是数字，阿里云的要求
         for(let key in log) {
             if(typeof log[key] === "number") {
                 log[key] = `${log[key]}`;
             }
         }
         console.log(log);
+        let body = JSON.stringify({
+            __logs__: [log]
+        });
         this.xhr.open("POST", this.url, true)
-        let body = JSON.stringify(log);
         this.xhr.setRequestHeader("Content-Type", "application/json");
         this.xhr.setRequestHeader("x-log-apiversion", "0.6.0");
         this.xhr.setRequestHeader("x-log-bodyrawsize", body.length);  // 请求体大小
         this.xhr.onload = function() {
-            console.log(this.xhr.response);
+            // console.log(this.xhr.response);
         }
         this.xhr.onerror = function(error) {
-            console.log(error);
+            // console.log(error);
         }
         this.xhr.send(body);
     }
